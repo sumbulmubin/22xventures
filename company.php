74554cf509987544d57db118ab2aa67e27386a7b
sumbulmubin <sumbul.mubin@janbask.com>
2024-08-30 17:06:05 +0530

    company

---
+++ company.php
@@ -0,0 +1,338 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+    <title>22x Ventures</title>
+    <meta content="" name="description">
+    <meta content="" name="keywords">
+
+
+
+    <?php include('inc/header.php') ?>
+
+    <!--  BANNER SLIDER  -->
+    <div class="company-section" style="background: #000; position: relative;">
+
+        <div class="container-fluid">
+
+            <div class="row">
+                <div class="col-lg-6 px-0 company-column-1" style="">
+                    <p class="sliderHeading text-white text-center">Innovation
+
+                    </p>
+                </div>
+                <div class="col-lg-6 px-0 company-column-2" style="">
+                    <p class="sliderHeading text-white text-center">Execution
+
+
+                    </p>
+                </div>
+            </div>
+
+
+        </div>
+    </div>
+    </div>
+
+    <style>
+        .company-section {
+            padding-top: 73px;
+        }
+
+        .company-column-1 {
+            background: linear-gradient(180deg, #00000096 38%, #00000096), url(https://img.freepik.com/premium-photo/colorful-school-supply-doodles-with-back-school-chalkboard-background_1160979-1453.jpg?w=740);
+            height: 70vh;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+        }
+
+        .company-column-2 {
+            background: linear-gradient(180deg, #00000096 38%, #00000096), url(https://img.freepik.com/free-photo/composition-different-objects-fathers-day_23-2148097968.jpg?t=st=1725004061~exp=1725007661~hmac=d341f2c2749e0908c05dd478f51cc6e411787609ce66cf188cab6ce9eab897e6&w=740);
+            height: 70vh;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+        }
+    </style>
+
+
+
+    <section style="background:#000; ">
+        <div class="container">
+            <div class="row justify-content-center ">
+                <div class="col-lg-8">
+                    <p class="sliderHeading2 text-white ">Company Summary
+                    </p>
+                    <p class="sliderSubHeading  text-white">We transform lives by securing financial security for the broad public.
+                    </p>
+                    <p class="sectionDescription text-white  mt-3 main-description-1">
+                        Lifestage Financial, LLC is a modern financial services company dedicated to democratizing wealth-building strategies and tools traditionally reserved for affluent families and businesses. The company's mission is to provide "Financial Freedom Access for All" through personalized, adaptable financial solutions catering to individuals at every life stage.
+
+                    </p>
+                </div>
+
+
+                <div class="col-lg-4 text-center" style="">
+
+                    <p class="sliderHeading2 text-white  ">Affiliates
+                    </p>
+
+                    <p>
+                     <a href=""><img src="assets/img/image6.png" alt="" class="img-fluid affiliate-img"></a>   
+                    </p>
+
+                    <p>
+                    <a href=""><img src="assets/img/image8.png" alt="" class="img-fluid affiliate-img"></a>    
+                    </p>
+
+                </div>
+            </div>
+        </div>
+    </section>
+<style>
+  .affiliate-img {
+    width: 144px;
+}
+</style>
+
+
+    <section style="background:#000;">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-8">
+
+                    <p class="sliderHeading2 text-white ">Offerings
+                    </p>
+                    <p class="sliderSubHeading  text-white">We take a holistic approach to deliver value, by offering a total workforce benefits bundle coupled with tier-one consulting.</p>
+
+                    <ol class="text-white offering-list">
+                        <li>Digital Family Office Kit: A comprehensive suite of wealth, financial, and risk management tools accessible to a broader audience.</li>
+                        <li>Personalized Financial Strategies: Tailored to each client's unique "Lifestage," offered complimentary.
+
+
+                        </li>
+                        <li>Workforce Benefits: Premium, digital-first solutions for businesses, including 401(k) plans, total rewards packages, and industry-first comprehensive contractor benefits.</li>
+                        <li>Specialized Financial Products: Including Principal Safe Fund, Secured Retirement Account, Tax-Free Income Account, and more.</li>
+                        <li>Business Services: Human capital strategies, fractional HR, training programs, and other growth-oriented solutions.</li>
+                    </ol>
+
+
+                
+
+                   
+                </div>
+                <div class="col-lg-4 text-center">
+                    <div class="d-flex align-items-center justify-content-center">
+                        <p class="sliderSubHeading text-white mb-0">Our Bundled Offerings </p>
+                        <img src="assets/img/two-hammers.png" alt="" class="img-fluid offering-img ms-2">
+                    </div>
+                    <div class="work-box-wrapper mb-3">
+                        <div class="work-box-offer d-flex align-items-center mt-4">
+                            <p class="sliderSubHeading text-center text-white pt-0 mb-0 px-3">EmployeesDigital Family Office Kit
+                            </p>
+
+                        </div>
+
+                        <p class="sectionDescription text-white  mt-3 main-description-1">
+                            Complimentary access to strategies and resources. </p>
+                    </div>
+
+                    <div class="work-box-wrapper mb-3">
+                        <div class="work-box-offer d-flex align-items-center mt-4">
+                            <p class="sliderSubHeading text-center text-white pt-0 mb-0 px-3">Employee Benefits
+                            </p>
+
+                        </div>
+
+                        <p class="sectionDescription text-white  mt-3 main-description-1">
+                            Digital 401(k), healthcare and life insurance, and more with comprehensive compliance.
+
+                        </p>
+                    </div>
+
+                    <div class="work-box-wrapper mb-3">
+                        <div class="work-box-offer d-flex align-items-center mt-4">
+                            <p class="sliderSubHeading text-center text-white pt-0 mb-0 px-3">Contractor Benefits
+
+                            </p>
+
+                        </div>
+
+                        <p class="sectionDescription text-white  mt-3 main-description-1">
+                            Lower cost of insurance, plus alt-wealth strategies.
+
+                        </p>
+                    </div>
+                   
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section style="background:#000;">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-8">
+
+                <p class="sliderHeading2 text-white mt-4 ">Differentiations
+
+                    </p>
+                    <p class="sliderSubHeading  text-white">Beyond our unique offerings, we are differentiated with our support of small businesses, communities, associations, and partners.</p>
+
+                    <ol class="text-white offering-list">
+                        <li>Tier-one management consulting, including fractional HR, CFO, COO services</li>
+                        <li>Integrated employee’s benefits delivered by marketing leading provider
+
+
+                        </li>
+                        <li>Comprehensive contractor benefits backed by large group captive insurance</li>
+                        <li>Complimentary access to a wide range of personal and business financial strategies</li>
+                        <li>Personalized approach based on individual "Lifestages”</li>
+                        <li>Focus on inclusive wealth-building for individuals and businesses
+                        </li>
+                        <li>Integration of traditional financial strategies with modern technology
+                        </li>
+                        <li>Delivering total workforce value including development and training</li>
+                    </ol>
+                    
+                   
+
+
+                </div>
+                <div class="col-lg-4 text-center">
+
+
+                   
+                   <div class="work-box-wrapper mb-3">
+                        <div class="work-box-offer d-flex align-items-center mt-4">
+                            <p class="sliderSubHeading text-center text-white pt-0 mb-0 px-3">SME’s Resources </p>
+
+                        </div>
+
+                        <p class="sectionDescription text-white  mt-3 main-description-1">
+                            Top-tier consulting and Shared Services BPO’s.</p>
+                    </div>
+                    <div class="work-box-wrapper mb-3">
+                        <div class="work-box-offer d-flex align-items-center mt-4">
+                            <p class="sliderSubHeading text-center text-white pt-0 mb-0 px-3">Principal Safe Funds
+                            </p>
+
+                        </div>
+
+                        <p class="sectionDescription text-white  mt-3 main-description-1">
+                            Alt-Asset Investments with Principal Protection.
+
+                        </p>
+                    </div>
+                    <div class="work-box-wrapper mb-3">
+                        <div class="work-box-offer d-flex align-items-center mt-4">
+                            <p class="sliderSubHeading text-center text-white pt-0 mb-0 px-3">Community Engagement</p>
+
+                        </div>
+
+                        <p class="sectionDescription text-white  mt-3 main-description-1">
+                            Empowering and investing in Impact Organizations. </p>
+                    </div>
+                  
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section style="background:#000;">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-8">
+
+                <p class="sliderSubHeading  text-white mt-4">Expertise</p>
+
+                <p class="sectionDescription text-white  ">Lifestage Financial provides access to subject matter experts across various fields, including financial planning, wealth strategy, insurance, tax mitigation, estate planning, and legal support.</p>
+
+                <p class="sliderSubHeading  text-white mt-2">Community Involvement</p>
+                <p class="sectionDescription text-white  ">The company invests in community initiatives, supporting non-profits, faith-based organizations, and the Asian-American leadership community through strategic investments and advisory services.</p>
+                <p class="sliderSubHeading  text-white mt-2">Partnership Approach</p>
+                <p class="sectionDescription text-white  ">As Complex Program Managers, Lifestage Financial leverages a partner-centric ecosystem, including fractional executives, management consultants, CPA firms, and legal experts, to create innovative, goal-aligned solutions for businesses.</p>
+                <p class="sectionDescription text-white  ">In summary, Lifestage Financial aims to transform the financial services landscape by making sophisticated wealth-building strategies accessible to a broader audience while providing comprehensive, personalized solutions for both individuals and businesses at every stage of their financial journey.</p>
+
+
+
+
+                </div>
+                <div class="col-lg-4 text-center">
+                    
+                <div class="work-box-wrapper mb-3">
+                        <div class="work-box-offer d-flex align-items-center mt-4">
+                            <p class="sliderSubHeading text-center text-white pt-0 mb-0 px-3">Partnerships
+
+
+                            </p>
+
+                        </div>
+
+                        <p class="sectionDescription text-white  mt-3 main-description-1">
+                            Affinity Marketing System; with Revenue Share.</p>
+                    </div>
+                    
+                  
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <style>
+        .work-box-wrapper .sectionDescription {
+            position: relative;
+        }
+
+        .work-box-wrapper .sectionDescription::after {
+            position: absolute;
+            content: '';
+            background-image: url(http://localhost/22xventures/assets/img/arrow-white.png);
+            top: 68px;
+            left: 63px;
+            width: 56px;
+            height: 100px;
+            background-size: contain;
+            background-repeat: no-repeat;
+        }
+
+        .work-box-wrapper .sectionDescription::before {
+            position: absolute;
+            content: 'Plus';
+            /* background-image: url(http://localhost/22xventures/assets/img/arrow-white.png); */
+            left: 124px;
+            top: 50%;
+            font-size: 30px;
+            color: #FFC107;
+        }
+
+        .work-box-wrapper .sectionDescription :nth-child(last)::before {
+            display: none;
+        }
+
+        .work-box-offer {
+            border: 2px solid #fff;
+            padding: 16px 5px 16px 5px;
+            width: 100%;
+            justify-content: center;
+            background: #000;
+        }
+
+        .offering-list {
+            padding-left: 15px;
+        }
+
+        .offering-list li {
+            margin-bottom: 5px;
+
+        }
+
+        .offering-img {
+            width: 20px;
+        }
+    </style>
+
+    <?php include('inc/footer.php') ?>
